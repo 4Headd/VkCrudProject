2a62cdcc3f0e068afd807eccfee2febe9946421f
4Headd <anikitin2508@mail.ru>
2023-05-10 17:17:08 +0300

    remove comment block

--- VkCrudProject/Context/UserContext.cs
+++ VkCrudProject/Context/UserContext.cs
@@ -22,33 +22,6 @@ namespace VkCrudProject.Context
                 Password = section.GetValue<string>("Password")
             };
 
-            /*modelBuilder.Entity<User>(eb =>
-            {
-                eb.Property(u => u.Id);
-                eb.Property(u => u.Login).IsRequired().HasMaxLength(20);
-                eb.Property(u => u.Password).IsRequired().HasMaxLength(16);
-                eb.Property(u => u.CreatedAt).IsRequired();
-                eb.Property(u => u.GroupId);
-                eb.Property(u => u.StateId);
-            }
-            );
-
-            modelBuilder.Entity<UserGroup>(eb =>
-            {
-                eb.Property(uG => uG.Id);
-                eb.Property(uG => uG.Role);
-                eb.Property(uG => uG.Description);
-            }
-            );
-
-            modelBuilder.Entity<UserState>(eb =>
-            {
-                eb.Property(uS => uS.Id);
-                eb.Property(uS => uS.Status);
-                eb.Property(uS => uS.Description);
-            }
-            );*/
-
             modelBuilder.Seed(admin);
         }
 
